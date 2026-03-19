821e6536a81b84527bbaa3c1adca6fff3b6c6a8d
Denilson Lehner <contato@denilsonlehner.com>
2026-03-19 10:46:55 -0300

    Updating wisp

--- Formula/wisp.rb
+++ Formula/wisp.rb
@@ -2,7 +2,7 @@ class Wisp < Formula
   desc "AI agent pipeline: PRDs to Pull Requests"
   homepage "https://github.com/delehner/wisp"
   license "MIT"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_intel do
